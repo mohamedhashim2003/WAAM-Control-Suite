9ccb7911bea1633dc98f7e0bb129b8adba1e3fa2
Mohamed Hashim <Mohamed.khaled.Hashim@outlook.com>
2026-02-27 09:58:16 -0800

    Create info.py

---
+++ info.py
@@ -0,0 +1,3 @@
+# WAAM Control System
+print("This repository contains the standalone executable for the WAAM Control System.")
+print("Please navigate to the 'Releases' tab on the right side of the screen to download the .exe file.")
